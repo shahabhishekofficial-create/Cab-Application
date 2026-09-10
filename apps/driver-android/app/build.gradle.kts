8ba20705d2e27f94a75418d122c0a5d85e270a2a
shahabhishekofficial-create <shahabhishekofficial@gmail.com>
2026-09-10 21:43:41 +0530

    Enable Room schema generation

--- apps/driver-android/app/build.gradle.kts
+++ apps/driver-android/app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose")
     id("org.jetbrains.kotlin.plugin.serialization")
     id("com.google.devtools.ksp")
+    id("androidx.room")
 }
 
 android {
