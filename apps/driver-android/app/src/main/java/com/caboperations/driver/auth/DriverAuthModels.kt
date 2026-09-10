a76a0f2309455cb06e4614b13de47a6472cb4d5b
shahabhishekofficial-create <shahabhishekofficial@gmail.com>
2026-09-10 21:38:48 +0530

    Restore DriverContext model

--- apps/driver-android/app/src/main/java/com/caboperations/driver/auth/DriverAuthModels.kt
+++ apps/driver-android/app/src/main/java/com/caboperations/driver/auth/DriverAuthModels.kt
@@ -1,3 +1,12 @@
 package com.caboperations.driver.auth
 
-// DriverContext is defined in DriverContextRepository.kt.
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class DriverContext(
+    val userId: String,
+    val driverId: String,
+    val displayName: String,
+    val vehicleId: String? = null,
+    val registrationNumber: String? = null
+)
